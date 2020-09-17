32909be90a8363a405468dde813461d2a0b91df6
hq <qihoo2017@gmail.com>
2020-09-17 11:28:52 +0800

    create lxdatacleaner version 0.0.2

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
-
+with open("README.md", "r",encoding='utf-8') as fh:
+  long_description = fh.read()
 setup(
-    name = 'lxdatacleaner',
-    version = '0.0.1',
+    name = 'lxkjdatacleaner',
+    version = '0.0.2',
     keywords='dataclean',
     description = 'a library for data clean',
     license = 'MIT License',
-    url = 'https://github.com/qihao/lxdatacleaner',
+    url = 'https://github.com/qihao123/lxdatacleaner',
     author = 'Hao Qi',
     author_email = '1047355811@qq.com',
     packages = find_packages(),
